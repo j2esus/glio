20b038a96b6a29d55f66934f36643c61a27cb94b
Jesús Vargas <jesus.vargas@jeegox.com>
2020-09-18 19:40:34 -0500

    Create unit tests for CategoryMenu entity

--- src/main/java/com/jeegox/glio/entities/admin/CategoryMenu.java
+++ src/main/java/com/jeegox/glio/entities/admin/CategoryMenu.java
@@ -1,5 +1,7 @@
 package com.jeegox.glio.entities.admin;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 import com.jeegox.glio.entities.util.JEntity;
 import com.jeegox.glio.enumerators.Status;
 import java.io.Serializable;
@@ -26,7 +28,20 @@ public class CategoryMenu extends JEntity<Integer> implements Serializable {
     private String icon;
     private String clazz;
     private List<OptionMenu> optionsMenus = new ArrayList<>();
-    
+
+    public CategoryMenu(){
+
+    }
+
+    public CategoryMenu(Integer id, String name, Integer order, Status status, String icon, String clazz) {
+        this.id = id;
+        this.name = name;
+        this.order = order;
+        this.status = status;
+        this.icon = icon;
+        this.clazz = clazz;
+    }
+
     @Id
     @Column(name = "id_category_menu")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -90,6 +105,34 @@ public class CategoryMenu extends JEntity<Integer> implements Serializable {
     public void setClazz(String clazz) {
         this.clazz = clazz;
     }
-    
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CategoryMenu)) return false;
+        CategoryMenu that = (CategoryMenu) o;
+        return Objects.equal(id, that.id) &&
+                Objects.equal(name, that.name) &&
+                Objects.equal(order, that.order) &&
+                status == that.status &&
+                Objects.equal(icon, that.icon) &&
+                Objects.equal(clazz, that.clazz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, name, order, status, icon, clazz);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("order", order)
+                .add("status", status)
+                .add("icon", icon)
+                .add("clazz", clazz)
+                .toString();
+    }
 }
---
+++ src/test/java/com/jeegox/glio/dao/CategoryMenuDAOTest.java
@@ -0,0 +1,99 @@
+package com.jeegox.glio.dao;
+
+import com.jeegox.glio.config.spring.ApplicationContextConfigTest;
+import com.jeegox.glio.dao.admin.CategoryMenuDAO;
+import com.jeegox.glio.entities.admin.CategoryMenu;
+import com.jeegox.glio.enumerators.Status;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.internal.SessionImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = ApplicationContextConfigTest.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class CategoryMenuDAOTest {
+    @Autowired
+    private SessionFactory sessionFactory;
+    @Autowired
+    private CategoryMenuDAO categoryMenuDAO;
+    private static CategoryMenu expectedCategoryMenu = new CategoryMenu(1, "Administración", 1,
+            Status.ACTIVE, "fa-building", "bg-primary");
+
+    @Before
+    public void setUp() throws SQLException{
+        insertInitialData();
+    }
+
+    @Test
+    public void findById_idExists_categoryMenu(){
+        assertThat(categoryMenuDAO.findById(1)).isEqualTo(expectedCategoryMenu);
+    }
+
+    @Test
+    public void findById_idNotExists_null(){
+        assertThat(categoryMenuDAO.findById(10)).isNull();
+    }
+
+    @Test
+    public void findAll_notRequired_categoryMenuList(){
+        assertThat(categoryMenuDAO.findAll()).isEqualTo(allExpectedCategories());
+    }
+
+    @Test
+    public void count_notRequired_totalNumbersOfRecords(){
+        assertThat(categoryMenuDAO.count()).isEqualTo(3);
+    }
+
+    @Test
+    public void exists_idExists_true(){
+        assertThat(categoryMenuDAO.exists(1)).isTrue();
+    }
+
+    @Test
+    public void exists_idNotExists_false(){
+        assertThat(categoryMenuDAO.exists(100)).isFalse();
+    }
+
+    private List<CategoryMenu> allExpectedCategories(){
+        List<CategoryMenu> categoryMenuList = new ArrayList<>();
+        categoryMenuList.add(new CategoryMenu(1, "Administración", 1, Status.ACTIVE, "fa-building", "bg-primary"));
+        categoryMenuList.add(new CategoryMenu(2, "Tareas", 2, Status.ACTIVE, "fa-list", "bg-warning"));
+        categoryMenuList.add(new CategoryMenu(3, "Gastos", 3, Status.ACTIVE, "fa-usd", "bg-success"));
+        return categoryMenuList;
+    }
+
+    private void insertInitialData() throws SQLException {
+        Session session = sessionFactory.getCurrentSession();
+        Connection connection = ((SessionImpl)session.getSession()).connection();
+
+        connection.createStatement().execute("insert into category_menu"+
+                "(id_category_menu, name, order_category_menu, status, icon, class) "+
+                "values "+
+                "(1, 'Administración', '1', 'ACTIVE', 'fa-building', 'bg-primary')");
+
+        connection.createStatement().execute("insert into category_menu"+
+                "(id_category_menu, name, order_category_menu, status, icon, class) "+
+                "values "+
+                "(2, 'Tareas', '2', 'ACTIVE', 'fa-list', 'bg-warning')");
+
+        connection.createStatement().execute("insert into category_menu"+
+                "(id_category_menu, name, order_category_menu, status, icon, class) "+
+                "values "+
+                "(3, 'Gastos', '3', 'ACTIVE', 'fa-usd', 'bg-success')");
+    }
+}
